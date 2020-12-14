6b46929ffd9acb3b5bfdba23485e166cff5dd967
austin_python <austinfeng@126.com>
2020-12-14 15:47:24 +0900

    '客户管理分支'

---
+++ src/views/skycrm/CusCustomerList.vue
@@ -0,0 +1,207 @@
+<template>
+  <a-card :bordered="false">
+    <!-- 查询区域 -->
+    <div class="table-page-search-wrapper">
+      <a-form layout="inline" @keyup.enter.native="searchQuery">
+        <a-row :gutter="24">
+        </a-row>
+      </a-form>
+    </div>
+    <!-- 查询区域-END -->
+
+    <!-- 操作按钮区域 -->
+    <div class="table-operator">
+      <a-button @click="handleAdd" type="primary" icon="plus">追加</a-button>
+      <a-button type="primary" icon="download" @click="handleExportXls('顧客情報')">エクスポート</a-button>
+      <a-upload name="file" :showUploadList="false" :multiple="false" :headers="tokenHeader" :action="importExcelUrl" @change="handleImportExcel">
+        <a-button type="primary" icon="import">インポート</a-button>
+      </a-upload>
+      <a-dropdown v-if="selectedRowKeys.length > 0">
+        <a-menu slot="overlay">
+          <a-menu-item key="1" @click="batchDel"><a-icon type="delete"/>削除</a-menu-item>
+        </a-menu>
+        <a-button style="margin-left: 8px"> 一括操作 <a-icon type="down" /></a-button>
+      </a-dropdown>
+    </div>
+
+    <!-- table区域-begin -->
+    <div>
+      <div class="ant-alert ant-alert-info" style="margin-bottom: 16px;">
+        <i class="anticon anticon-info-circle ant-alert-icon"></i> 選択済み <a style="font-weight: 600">{{ selectedRowKeys.length }}</a>行
+        <a style="margin-left: 24px" @click="onClearSelected">クリア</a>
+      </div>
+
+      <a-table
+        ref="table"
+        size="middle"
+        :scroll="{x:true}"
+        bordered
+        rowKey="id"
+        :columns="columns"
+        :dataSource="dataSource"
+        :pagination="ipagination"
+        :loading="loading"
+        :rowSelection="{selectedRowKeys: selectedRowKeys, onChange: onSelectChange}"
+        class="j-table-force-nowrap"
+        @change="handleTableChange">
+
+        <template slot="htmlSlot" slot-scope="text">
+          <div v-html="text"></div>
+        </template>
+        <template slot="imgSlot" slot-scope="text">
+          <span v-if="!text" style="font-size: 12px;font-style: italic;">写真なし</span>
+          <img v-else :src="getImgView(text)" height="25px" alt="" style="max-width:80px;font-size: 12px;font-style: italic;"/>
+        </template>
+        <template slot="fileSlot" slot-scope="text">
+          <span v-if="!text" style="font-size: 12px;font-style: italic;">ファイルなし</span>
+          <a-button
+            v-else
+            :ghost="true"
+            type="primary"
+            icon="download"
+            size="small"
+            @click="downloadFile(text)">
+            ダウンロード
+          </a-button>
+        </template>
+
+        <span slot="action" slot-scope="text, record">
+          <a @click="handleEdit(record)">編集</a>
+
+          <a-divider type="vertical" />
+          <a-dropdown>
+            <a class="ant-dropdown-link">最も <a-icon type="down" /></a>
+            <a-menu slot="overlay">
+              <a-menu-item>
+                <a @click="handleDetail(record)">詳細情報</a>
+              </a-menu-item>
+              <a-menu-item>
+                <a-popconfirm title="削除しますか？" @confirm="() => handleDelete(record.id)">
+                  <a>削除</a>
+                </a-popconfirm>
+              </a-menu-item>
+            </a-menu>
+          </a-dropdown>
+        </span>
+
+      </a-table>
+    </div>
+
+    <cus-customer-modal ref="modalForm" @ok="modalFormOk"></cus-customer-modal>
+  </a-card>
+</template>
+
+<script>
+
+  import '@/assets/less/TableExpand.less'
+  import { mixinDevice } from '@/utils/mixin'
+  import { JeecgListMixin } from '@/mixins/JeecgListMixin'
+  import CusCustomerModal from './modules/CusCustomerModal'
+
+  export default {
+    name: 'CusCustomerList',
+    mixins:[JeecgListMixin, mixinDevice],
+    components: {
+      CusCustomerModal
+    },
+    data () {
+      return {
+        description: '顧客情報管理页面',
+        // 表头
+        columns: [
+          {
+            title: '#',
+            dataIndex: '',
+            key:'rowIndex',
+            width:60,
+            align:"center",
+            customRender:function (t,r,index) {
+              return parseInt(index)+1;
+            }
+          },
+          {
+            title:'顧客ID',
+            align:"center",
+            dataIndex: 'cusId'
+          },
+          {
+            title:'顧客名',
+            align:"center",
+            dataIndex: 'cusName'
+          },
+          {
+            title:'会社住所',
+            align:"center",
+            dataIndex: 'cusAddress'
+          },
+          {
+            title:'連絡先',
+            align:"center",
+            dataIndex: 'cusPername'
+          },
+          {
+            title:'電話番号',
+            align:"center",
+            dataIndex: 'cusPhonenum'
+          },
+          {
+            title:'項目分類',
+            align:"center",
+            dataIndex: 'cusSort_dictText'
+          },
+          {
+            title:'顧客級別',
+            align:"center",
+            dataIndex: 'cusSen_dictText'
+          },
+          {
+            title:'顧客タイプ',
+            align:"center",
+            dataIndex: 'cusType'
+          },
+          {
+            title:'項目内容',
+            align:"center",
+            dataIndex: 'cusHeadline'
+          },
+          {
+            title:'備考',
+            align:"center",
+            dataIndex: 'cusMemos'
+          },
+          {
+            title: '操作',
+            dataIndex: 'action',
+            align:"center",
+            fixed:"right",
+            width:147,
+            scopedSlots: { customRender: 'action' }
+          }
+        ],
+        url: {
+          list: "/ko/cusCustomer/list",
+          delete: "/ko/cusCustomer/delete",
+          deleteBatch: "/ko/cusCustomer/deleteBatch",
+          exportXlsUrl: "/ko/cusCustomer/exportXls",
+          importExcelUrl: "ko/cusCustomer/importExcel",
+
+        },
+        dictOptions:{},
+      }
+    },
+    created() {
+    },
+    computed: {
+      importExcelUrl: function(){
+        return `${window._CONFIG['domianURL']}/${this.url.importExcelUrl}`;
+      },
+    },
+    methods: {
+      initDictConfig(){
+      }
+    }
+  }
+</script>
+<style scoped>
+  @import '~@assets/less/common.less';
+</style>
---
+++ src/views/skycrm/modules/CusCustomerForm.vue
@@ -0,0 +1,213 @@
+<template>
+  <a-spin :spinning="confirmLoading">
+    <j-form-container :disabled="formDisabled">
+      <a-form :form="form" slot="detail">
+        <a-row>
+          <a-col :span="24">
+            <a-form-item label="顧客ID" :labelCol="labelCol" :wrapperCol="wrapperCol">
+              <a-input v-decorator="['cusId', validatorRules.cusId]" placeholder="请输入顧客ID"></a-input>
+            </a-form-item>
+          </a-col>
+          <a-col :span="24">
+            <a-form-item label="顧客名" :labelCol="labelCol" :wrapperCol="wrapperCol">
+              <a-input v-decorator="['cusName', validatorRules.cusName]" placeholder="请输入顧客名"></a-input>
+            </a-form-item>
+          </a-col>
+          <a-col :span="24">
+            <a-form-item label="会社住所" :labelCol="labelCol" :wrapperCol="wrapperCol">
+              <a-input v-decorator="['cusAddress']" placeholder="请输入会社住所"></a-input>
+            </a-form-item>
+          </a-col>
+          <a-col :span="24">
+            <a-form-item label="連絡先" :labelCol="labelCol" :wrapperCol="wrapperCol">
+              <a-input v-decorator="['cusPername']" placeholder="请输入連絡先"></a-input>
+            </a-form-item>
+          </a-col>
+          <a-col :span="24">
+            <a-form-item label="電話番号" :labelCol="labelCol" :wrapperCol="wrapperCol">
+              <a-input v-decorator="['cusPhonenum']" placeholder="请输入電話番号"></a-input>
+            </a-form-item>
+          </a-col>
+          <a-col :span="24">
+            <a-form-item label="項目分類" :labelCol="labelCol" :wrapperCol="wrapperCol">
+              <j-dict-select-tag type="list" v-decorator="['cusSort']" :trigger-change="true" dictCode="" placeholder="请选择項目分類"/>
+            </a-form-item>
+          </a-col>
+          <a-col :span="24">
+            <a-form-item label="顧客級別" :labelCol="labelCol" :wrapperCol="wrapperCol">
+              <j-dict-select-tag type="list" v-decorator="['cusSen']" :trigger-change="true" dictCode="" placeholder="请选择顧客級別"/>
+            </a-form-item>
+          </a-col>
+          <a-col :span="24">
+            <a-form-item label="顧客タイプ" :labelCol="labelCol" :wrapperCol="wrapperCol">
+              <a-input v-decorator="['cusType']" placeholder="请输入顧客タイプ"></a-input>
+            </a-form-item>
+          </a-col>
+          <a-col :span="24">
+            <a-form-item label="項目内容" :labelCol="labelCol" :wrapperCol="wrapperCol">
+              <a-input v-decorator="['cusHeadline']" placeholder="请输入項目内容"></a-input>
+            </a-form-item>
+          </a-col>
+          <a-col :span="24">
+            <a-form-item label="備考" :labelCol="labelCol" :wrapperCol="wrapperCol">
+              <a-input v-decorator="['cusMemos']" placeholder="请输入備考"></a-input>
+            </a-form-item>
+          </a-col>
+          <a-col v-if="showFlowSubmitButton" :span="24" style="text-align: center">
+            <a-button @click="submitForm">提 交</a-button>
+          </a-col>
+        </a-row>
+      </a-form>
+    </j-form-container>
+  </a-spin>
+</template>
+
+<script>
+
+  import { httpAction, getAction } from '@/api/manage'
+  import pick from 'lodash.pick'
+  import { validateDuplicateValue } from '@/utils/util'
+  import JFormContainer from '@/components/jeecg/JFormContainer'
+  import JDictSelectTag from "@/components/dict/JDictSelectTag"
+
+  export default {
+    name: 'CusCustomerForm',
+    components: {
+      JFormContainer,
+      JDictSelectTag,
+    },
+    props: {
+      //流程表单data
+      formData: {
+        type: Object,
+        default: ()=>{},
+        required: false
+      },
+      //表单模式：true流程表单 false普通表单
+      formBpm: {
+        type: Boolean,
+        default: false,
+        required: false
+      },
+      //表单禁用
+      disabled: {
+        type: Boolean,
+        default: false,
+        required: false
+      }
+    },
+    data () {
+      return {
+        form: this.$form.createForm(this),
+        model: {},
+        labelCol: {
+          xs: { span: 24 },
+          sm: { span: 5 },
+        },
+        wrapperCol: {
+          xs: { span: 24 },
+          sm: { span: 16 },
+        },
+        confirmLoading: false,
+        validatorRules: {
+          cusId: {
+            rules: [
+              { required: true, message: '请输入顧客ID!'},
+            ]
+          },
+          cusName: {
+            rules: [
+              { required: true, message: '请输入顧客名!'},
+            ]
+          },
+        },
+        url: {
+          add: "/ko/cusCustomer/add",
+          edit: "/ko/cusCustomer/edit",
+          queryById: "/ko/cusCustomer/queryById"
+        }
+      }
+    },
+    computed: {
+      formDisabled(){
+        if(this.formBpm===true){
+          if(this.formData.disabled===false){
+            return false
+          }
+          return true
+        }
+        return this.disabled
+      },
+      showFlowSubmitButton(){
+        if(this.formBpm===true){
+          if(this.formData.disabled===false){
+            return true
+          }
+        }
+        return false
+      }
+    },
+    created () {
+      //如果是流程中表单，则需要加载流程表单data
+      this.showFlowData();
+    },
+    methods: {
+      add () {
+        this.edit({});
+      },
+      edit (record) {
+        this.form.resetFields();
+        this.model = Object.assign({}, record);
+        this.visible = true;
+        this.$nextTick(() => {
+          this.form.setFieldsValue(pick(this.model,'cusId','cusName','cusAddress','cusPername','cusPhonenum','cusSort','cusSen','cusType','cusHeadline','cusMemos'))
+        })
+      },
+      //渲染流程表单数据
+      showFlowData(){
+        if(this.formBpm === true){
+          let params = {id:this.formData.dataId};
+          getAction(this.url.queryById,params).then((res)=>{
+            if(res.success){
+              this.edit (res.result);
+            }
+          });
+        }
+      },
+      submitForm () {
+        const that = this;
+        // 触发表单验证
+        this.form.validateFields((err, values) => {
+          if (!err) {
+            that.confirmLoading = true;
+            let httpurl = '';
+            let method = '';
+            if(!this.model.id){
+              httpurl+=this.url.add;
+              method = 'post';
+            }else{
+              httpurl+=this.url.edit;
+               method = 'put';
+            }
+            let formData = Object.assign(this.model, values);
+            console.log("表单提交数据",formData)
+            httpAction(httpurl,formData,method).then((res)=>{
+              if(res.success){
+                that.$message.success(res.message);
+                that.$emit('ok');
+              }else{
+                that.$message.warning(res.message);
+              }
+            }).finally(() => {
+              that.confirmLoading = false;
+            })
+          }
+         
+        })
+      },
+      popupCallback(row){
+        this.form.setFieldsValue(pick(row,'cusId','cusName','cusAddress','cusPername','cusPhonenum','cusSort','cusSen','cusType','cusHeadline','cusMemos'))
+      },
+    }
+  }
+</script>
